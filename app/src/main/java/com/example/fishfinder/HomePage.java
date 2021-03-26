09bdf19b5eaba72cca4d8873cb8ffc156dc73cfe
hacth234 <hacth234@gmail.com>
2021-03-26 17:01:08 -0400

    fixed string error

--- app/src/main/java/com/example/fishfinder/HomePage.java
+++ app/src/main/java/com/example/fishfinder/HomePage.java
@@ -1,4 +1,19 @@
 package com.example.fishfinder;
 
-public class HomePage {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class HomePage extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.homepage);
+    }
+
 }
--- app/src/main/java/com/example/fishfinder/MainActivity.java
+++ app/src/main/java/com/example/fishfinder/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.fishfinder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button bntSignIn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bntSignIn = (Button) findViewById(R.id.btnSignIn);
+
+        bntSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), HomePage.class);
+
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
