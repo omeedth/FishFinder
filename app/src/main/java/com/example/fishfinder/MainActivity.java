b86ef092400b5ac3b4239fd31933f6ee6a787635
JoshuaPei <joshuapei@Joshuas-MacBook-Pro-2.local>
2021-03-26 17:57:18 -0400

    main page app

--- app/src/main/java/com/example/fishfinder/MainActivity.java
+++ app/src/main/java/com/example/fishfinder/MainActivity.java
@@ -1,14 +1,32 @@
-package com.example.fishfinder;
+package com.example.a501projectmainpage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Components
+    private Button btnSearchFish;
+    private TextView tvMainPage;
+    private Button btnGoToProfile;
+    private ListView lvCommunity;
+    private Button btnLogout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSearchFish = (Button) findViewById(R.id.btnSearchFish);
+        tvMainPage = (TextView) findViewById(R.id.tvMainPage);
+        btnGoToProfile = (Button) findViewById(R.id.btnGoToProfile);
+        lvCommunity = (ListView) findViewById(R.id.lvCommunity);
+        btnLogout = (Button) findViewById(R.id.btnLogout);
+
     }
 }
