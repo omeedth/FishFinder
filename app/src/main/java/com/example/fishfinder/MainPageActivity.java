fd05686e2f81374217539a496ee67552d73a0aa8
hacth234 <hacth234@gmail.com>
2021-04-05 00:38:37 -0400

    Added settings page, profile page and thier intent

--- app/src/main/java/com/example/fishfinder/MainPageActivity.java
+++ app/src/main/java/com/example/fishfinder/MainPageActivity.java
@@ -1,11 +1,16 @@
 package com.example.fishfinder;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainPageActivity extends AppCompatActivity {
@@ -42,5 +47,42 @@ public class MainPageActivity extends AppCompatActivity {
             }
         });
 
+        btnGoToProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent goToProfile = new Intent(v.getContext(), UserProfileActivity.class);
+                startActivity(goToProfile);
+
+            }
+        });
+
+
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+//        return super.onCreateOptionsMenu(menu);   //get rid of default behavior.
+
+        // Inflate the menu; this adds items to the action bar
+        getMenuInflater().inflate(R.menu.simple_menu, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id == R.id.mnu_zero) { //replacing given menu code with new ones
+            Toast.makeText(getBaseContext(), "Settings", Toast.LENGTH_LONG).show();
+            Intent goToSettings = new Intent(getBaseContext(), SettingsPageActivity.class);
+            startActivity(goToSettings);
+
+        }
+
+            return true;
+        }
 }
