5f29e3a517002e782100476faeadfcc94c139e86
omeedth <aothomas@bu.edu>
2021-04-11 13:25:48 -0400

    Cloned Master (no changes)

---
+++ app/src/main/java/com/example/fishfinder/data/GeneralTest.java
@@ -0,0 +1,70 @@
+package com.example.fishfinder.data;
+
+//a test save class to save to database
+public class GeneralTest {
+
+    String userId;
+    String email;
+
+    String title;
+    String latitude;
+    String longitude;
+    String imgId; //the number + .jpg in the storagereference database
+
+
+    public GeneralTest() {
+        this.title = "";
+        this.latitude  = "";
+        this.longitude = "";
+        this.userId = "";
+        this.email = "";
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getImgId() {
+        return imgId;
+    }
+
+    public void setImgId(String imgId) {
+        this.imgId = imgId;
+    }
+}
